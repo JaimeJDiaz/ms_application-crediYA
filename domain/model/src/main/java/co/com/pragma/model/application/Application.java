d7c1c3960dc0702d57bf332289dbec25e263800e
Torneyus <jjd21017@gmail.com>
2025-09-03 10:25:53 -0500

    Initial commit

---
+++ domain/model/src/main/java/co/com/pragma/model/application/Application.java
@@ -0,0 +1,22 @@
+package co.com.pragma.model.application;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Application {
+    private BigInteger id;
+    private BigDecimal amount;
+    private String type;
+    private BigInteger userId;
+    private String status;
+}
